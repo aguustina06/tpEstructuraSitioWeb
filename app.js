aa54ba951bda75dcf20afe1e8ca09a9492003d4e
aguustina06 <agustina_0610@hotmail.com>
2021-09-02 18:10:32 -0300

    login y registro

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, './views/home.html'))
 })
 
+app.get('/login', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+})
+
+app.get('/registro', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/registro.html'))
+})
+
 app.listen(port, () => {
     console.log("servidor funcionando") + port
 })
